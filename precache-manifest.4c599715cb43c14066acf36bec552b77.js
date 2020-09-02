00b844348e83650eee23d342dc9efcc6e9d0c03d
BeissenMich <blackyellovv@gmail.com>
2020-09-02 15:05:18 +0300

    Updates

---
+++ precache-manifest.4c599715cb43c14066acf36bec552b77.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7618f0e82ad4179c73e2302aaac1b2",
+    "url": "/app-feed-the-cat/index.html"
+  },
+  {
+    "revision": "e22b046894e90b75f215",
+    "url": "/app-feed-the-cat/static/css/main.4e3493e1.chunk.css"
+  },
+  {
+    "revision": "3c7a30334afaeefb4b57",
+    "url": "/app-feed-the-cat/static/js/2.92e60d9d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/app-feed-the-cat/static/js/2.92e60d9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22b046894e90b75f215",
+    "url": "/app-feed-the-cat/static/js/main.5e695c2f.chunk.js"
+  },
+  {
+    "revision": "9d614bc1d4177f008e88",
+    "url": "/app-feed-the-cat/static/js/runtime-main.1dc64b50.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/app-feed-the-cat/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "8c1310d74df3b4c7ea0a523ca525ab01",
+    "url": "/app-feed-the-cat/static/media/cat.8c1310d7.png"
+  },
+  {
+    "revision": "98ed80041ab53dce8fa104d6b5c296e1",
+    "url": "/app-feed-the-cat/static/media/fon.98ed8004.jpg"
+  }
+]);
